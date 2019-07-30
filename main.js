09be1f57d852ad6901076a287a7e7b2a4ccab817
Dalen Romelien <43348491+dromelie@users.noreply.github.com>
2019-07-30 14:00:35 -0700

    Add files via upload
    Hard coded way points

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var name = document.getElementById("name");
+var longitude = document.getElementById("longitude");
+var latitude = document.getElementById("latitude");
+
 var sidepane = new Vue({
     el: '#side-pane',
     data:{
@@ -11,12 +15,28 @@ var sidepane = new Vue({
                 name: "B",
                 long: 34.832112,
                 lat: -98.138652
+            },
+            {
+                name: "C",
+                long: -38.9463256,
+                lat: 7.865736
+            },
+            {
+                name: "D",
+                long: 12.744729,
+                lat: 7.518548
+            },
+            {
+                name: "E",
+                long: -87.523898,
+                lat: 97.868758
             }
         ]
     },
     methods: {
         addWayPoint: function(){
-
+            waypoints.push({name: this.name, long: this.longitude, lat: this.latitude})
+            document.getElementById("myForm").reset();
         }
     }
 })
