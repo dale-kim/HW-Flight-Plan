a5353d3d23a9d5a7ef0919d81354fd1e5634870b
dale-kim <dalekim1109@gmail.com>
2019-07-30 10:51:55 -0700

    Fixed folder structure

---
+++ Backend/queries.js
@@ -0,0 +1,55 @@
+/* MONGOOSE SETUP */
+const mongoose = require('mongoose')
+const uri = 'mongodb://127.0.0.1:27017/flight-plans'
+mongoose.connect(uri, {useNewUrlParser: true})
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${uri}`)
+})
+mongoose.connection.on('error', err => {
+    console.log('Mongoose connection error:', err)
+})
+const planeSchema = new mongoose.Schema({ 
+    name: String, 
+    tail: String,
+    date: String, 
+    condition: String 
+})
+const planes = mongoose.model('planes', planeSchema)
+
+/* CALLBACK DEFINES */
+const findByName = (req, res) => {
+    planes.findOne({name: req.params.name}, (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results)
+    })
+}
+
+const removePlane = (req, res) => {
+    Plane.deleteOne({name: req.params.name}, (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results)
+    })
+}
+
+const addPlane = (req, res) => {
+    const newPlane = {
+        name: req.body.name,
+        tail: req.body.tail,
+        date: req.body.date,
+        condition: req.body.condition
+    }
+    mongoose.connection.collection('planes').insertOne(newPlane), (error, res)=> {
+        if (error) throw error
+    }
+    res.status(200).send('Successfully added new plane')
+}
+
+module.exports = {
+    findByName,
+    addPlane,
+    removePlane
+}
