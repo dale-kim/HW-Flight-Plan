68424f01d4e72f74921c372b4e43a9171bc433af
dale-kim <dalekim1109@gmail.com>
2019-07-30 20:41:35 -0700

    Cleaned code, added weather API. Added static waypoints.

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 const db = require('./queries')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(
@@ -11,18 +10,17 @@ app.use(
     })
 )
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
-
+  });
 
 // /* CALLBACK DEFINES */
 app.get('/', (request, response) => {
     response.json({ info: 'Welcome to Honeywell Flight Plan app!' })
 })
-app.get('/findbyname/:name', db.findByName)
+app.get('/getweatherbycity/:lon/:lat', db.getWeatherByCity)
 app.get('/removeplane/:name', db.removePlane)
 app.get('/getallplanes', db.getAllPlanes)
 app.post('/addplane', db.addPlane)
--- Backend/queries.js
+++ Backend/queries.js
@@ -1,24 +1,39 @@
 /* MONGOOSE SETUP */
+const request = require('request')
 const mongoose = require('mongoose')
 const uri = 'mongodb://127.0.0.1:27017/flight-plans'
-mongoose.connect(uri, {useNewUrlParser: true})
+mongoose.connect(uri, { useNewUrlParser: true })
 mongoose.connection.on('connected', () => {
     console.log(`Mongoose connected to ${uri}`)
 })
 mongoose.connection.on('error', err => {
     console.log('Mongoose connection error:', err)
 })
-const planeSchema = new mongoose.Schema({ 
-    name: String, 
+const planeSchema = new mongoose.Schema({
+    name: String,
     tail: String,
-    date: [String], 
-    condition: String 
+    date: [String],
+    condition: String
 })
 const planes = mongoose.model('planes', planeSchema)
 
 /* CALLBACK DEFINES */
+const getWeatherByCity = (req, res) => {
+    request('http://api.openweathermap.org/data/2.5/weather?lat=' + req.params.lat + '&lon=' + req.params.lon + '&APPID=31d22a26b3aa523236523eac655eac09', function (error, response, body) {
+        var description = JSON.parse(body).weather[0].description
+        var wind = JSON.parse(body).wind
+        var data = {
+            "description": description,
+            "wind-speed": (wind.speed).toString() + " m/s",
+            "wind-degrees": (wind.deg).toString()
+        }
+        res.json(data);
+    });
+}
+
+
 const findByName = (req, res) => {
-    planes.findOne({name: req.params.name}, (error, results) => {
+    planes.findOne({ name: req.params.name }, (error, results) => {
         if (error) {
             throw error
         }
@@ -36,7 +51,7 @@ const getAllPlanes = (req, res) => {
 }
 
 const removePlane = (req, res) => {
-    planes.deleteOne({name: req.params.name}, (error, results) => {
+    planes.deleteOne({ name: req.params.name }, (error, results) => {
         if (error) {
             throw error
         }
@@ -52,14 +67,14 @@ const addPlane = (req, res) => {
         date: req.body.date,
         condition: req.body.condition
     }
-    mongoose.connection.collection('planes').insertOne(newPlane), (error, res)=> {
+    mongoose.connection.collection('planes').insertOne(newPlane), (error, res) => {
         if (error) throw error
     }
     res.status(200).send('Successfully added new plane')
 }
 
 const updatePlane = (req, res) => {
-    mongoose.connection.collection('planes').findOneAndUpdate({name: req.body.name}, {$push:{date:req.body.date}}, (err, doc) => {
+    mongoose.connection.collection('planes').findOneAndUpdate({ name: req.body.name }, { $push: { date: req.body.date } }, (err, doc) => {
         if (err) {
             console.log("Something went wrong when updating data!");
         }
@@ -73,11 +88,18 @@ module.exports = {
     removePlane,
     getAllPlanes,
     updatePlane,
+    getWeatherByCity
 }
 
 // Static pre-inserted mock database
 // db.planes.insertOne({"name":"Boeing 777x","tail":"D-ABCE","date":["July 4 2019"],"condition":"good"})
 // db.planes.insertOne({"name":"Airbus A380","tail":"W-XYZ","date":["December 25 2018"],"condition":"moderate"})
 // db.planes.insertOne({"name":"Boeing 737 MAX","tail":"666","date":["April 20 1962"],"condition":"bad"})
+
+// db.planes.insertOne({"airport":"LAX", "lonlat":["-118.4085", "33.9427"]})
+// db.planes.insertOne({"airport":"PHX", "lonlat":["-111.9186", "32.9908"]})
+// db.planes.insertOne({"airport":"AUS", "lonlat":["-97.6686", "30.1899"]})
+// db.planes.insertOne({"airport":"ATL", "lonlat":["-84.419853", "33.6404"]})
+// db.planes.insertOne({"airport":"JFK", "lonlat":["-73.7781", "40.6413"]})
 
 
